9748ad3914b5799eecacf892e9a3fb45df13adc5
Allan Yanik <allan.carvalho@webjump.com.br>
2020-12-03 10:59:14 -0300

    Adds response to state and styles response

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import StyledMain from './mainStyle'
 import api from '../../services/api'
 
 const Main = () => {
+
+  const [products, setProducts] = useState()
   const loadProducts = async () => {
     const response = await api.get(`/products`)
     const { docs: productData } = response.data
+
+    if (productData) {
+      setProducts(productData)
+    }
 
     return productData
   }
@@ -16,7 +22,17 @@ const Main = () => {
 
   return (
     <StyledMain>
-      Carregando...
+      {products && products.map(product => {
+        const { _id: id, title, description} = product;
+
+        return(
+          <article key={id}>
+            <strong>{title}</strong>
+            <p>{description}</p>
+            <a href="#">Access</a>
+          </article>
+        )
+      })}
     </StyledMain>
   )
 }
