4073d2ba61821207e68fc52059b092d32aac5824
Allan Yanik <allan.carvalho@webjump.com.br>
2020-12-03 10:22:52 -0300

    Creates header

---
+++ src/components/Header/headerStyle.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+  height: 60px;
+  background: #da552f;
+  font-size: 18px;
+  font-weight: bold;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default StyledHeader
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import StyledHeader from './headerStyle'
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <p>JSHunt</p>
+    </StyledHeader>
+  )
+}
+
+export default Header
