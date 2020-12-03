47785b09cfd67e074394a8e6560bd669b6081e2b
Allan Yanik <allan.carvalho@webjump.com.br>
2020-12-03 11:25:26 -0300

    Creates basic routing

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import StyledMain, { Navigation } from './mainStyle'
 import api from '../../services/api'
 
@@ -52,7 +53,7 @@ const Main = () => {
           <article key={id}>
             <strong>{title}</strong>
             <p>{description}</p>
-            <a href="#">Access</a>
+            <Link to={`/products/${id}`}>Access</Link>
           </article>
         )
       })}
