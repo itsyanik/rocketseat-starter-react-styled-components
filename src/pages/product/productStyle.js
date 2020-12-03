45d8a76e7d9529e181686c898eff085abac3a7a5
Allan Yanik <allan.carvalho@webjump.com.br>
2020-12-03 11:37:01 -0300

    Creates product page

---
+++ src/pages/product/productStyle.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+const ProductPage = styled.div`
+  max-width: 700px;
+  margin: 20px auto 0;
+  padding: 20px;
+  background: #fff;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+
+  h1 {
+    font-size: 32px;
+  }
+
+  p {
+    color: #666;
+    line-height: 24px;
+    margin-top: 5px;
+
+    a {
+      color: #069;
+    }
+  }
+`
+
+export default ProductPage
